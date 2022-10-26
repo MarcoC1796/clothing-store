05c5929d6d903ddb042c275b24b43f2464aa2ac7
Marco Chacon <marco.chacon1796@gmail.com>
2022-10-26 15:45:12 -0500

    adding persistent navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
 const Shop = () => {
-  return <h1>I am the Shop page</h1>;
+  return <h1> I am the shop page</h1>;
 };
 
 const App = () => {
@@ -11,8 +22,10 @@ const App = () => {
     // Nested Paths
     // Parental components will render always unless told otherwise
     // To render child path route components we need to sepcify it inside the parent component
+    // index set to true means that the child route will also render along the parent route
     <Routes>
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>

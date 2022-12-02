ed17dbd2c27a974159ea237d8b007d470aeba492
Marco Chacon <marco.chacon1796@gmail.com>
2022-12-02 00:31:56 -0600

    adding Category dynamic route and component

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,32 @@
+import { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { CategoriesContext } from "../../contexts/categories.context";
+
+import ProductCard from "../../components/product-card/product-card.component";
+
+import "./category.style.scss";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  // will set products to an empty object at the beggining
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  // We use the products && to protect against the async code when fetching products
+  return (
+    <div className="category-container">
+      {products &&
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+    </div>
+  );
+};
+
+export default Category;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import CategoriesPreview from "../categories-preview/categories-preview.component";
 
-import "./shop.style.scss";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 const Shop = () => {
+  // a path like :category means a dynamic route that can take a value
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
